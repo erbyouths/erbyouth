e8bb64ddba589ef7bb44740d8549f09e48709888
ElvinLong <970099734@qq.com>
2016-03-14 17:23:51 +0800

    修改路由

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'erbyout 首页' });
 });
 
+/* GET home page. */
+router.get('/landed', function(req, res, next) {
+  res.render('landed', { title: 'erbyout landed' });
+});
+
 module.exports = router;
