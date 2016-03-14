0861d9a6cf5d084000517a7797adab1aaf7adf7d
jack-xiong <1101106641@qq.com>
2016-03-14 22:35:29 +0800

    connect mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var blog = require('./models/blog');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'erbyout 首页' });
+	 blog.fetch(function(obj) {
+		 res.render('index', { title: 'erbyout 首页'});
+ 
+	 	});
+
 });
 
 /* GET home page. */
